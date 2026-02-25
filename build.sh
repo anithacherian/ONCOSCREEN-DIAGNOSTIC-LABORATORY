856b8fa7325c8662c468e4de0e3d1d2bd8ce71d1
anithacherian <anitha.cher20@gmail.com>
2026-02-25 21:14:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
+pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
